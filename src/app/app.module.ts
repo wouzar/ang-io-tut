edac389962d808b02418e323774238de25b3047c
wouzar <wouzar@gmail.com>
2017-06-16 08:41:23 +0300

    Replace mocked data with in-memory web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { HeroesComponent }  from './heroes.component';
-import {HeroDetailComponent} from "./hero-detail.component";
-import {AppComponent} from "./app.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+import { AppComponent } from "./app.component";
 import { HeroService } from "./hero.service";
 import { DashboardComponent } from "./dashboard.component";
 
 import {AppRoutingModule} from "./app-routing.module";
+import { HttpModule } from "@angular/http";
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [
     AppComponent,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Hero } from "./hero";
-import { HEROES } from "./mock-heroes";
+import 'rxjs/add/operator/toPromise';
+import { Http } from "@angular/http";
 
 @Injectable()
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) { }
+
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(resp => resp.json().data as Hero)
+      .catch(this.handleError);
   }
 
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get("/api/heroes")
+      .toPromise()
+      .then(resp => resp.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
